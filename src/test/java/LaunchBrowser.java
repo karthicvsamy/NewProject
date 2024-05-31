5d6675cd3d227e43693fa783025ed10e0f9c570c
karthicvsamy <karthicvsamy@gmail.com>
2024-05-31 15:11:44 +0530

    first time commit

---
+++ src/test/java/LaunchBrowser.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LaunchBrowser {
+	
+	@Test
+	public void firstTime() {
+		WebDriver driver = new ChromeDriver();
+	}
+}
