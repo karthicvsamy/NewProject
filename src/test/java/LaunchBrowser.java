03f9547bcf26ada406a13ae5e6acfc5b8d8d5c2b
karthicvsamy <karthicvsamy@gmail.com>
2024-05-31 15:14:42 +0530

    second time commit

--- src/test/java/LaunchBrowser.java
+++ src/test/java/LaunchBrowser.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class LaunchBrowser {
@@ -7,5 +8,6 @@ public class LaunchBrowser {
 	@Test
 	public void firstTime() {
 		WebDriver driver = new ChromeDriver();
+		Reporter.log("Successfully launched", true);
 	}
 }
